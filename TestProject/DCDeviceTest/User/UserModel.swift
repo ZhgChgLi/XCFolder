cc2c251650e22ca59e25caf56fdb9fc558bfc8a7
ZhgChgLiBot <no-reply@zhgchg.li>
2025-03-02 16:59:49 +0800

    reset commit

--- TestProject/DCDeviceTest/User/UserModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserModel.swift
-//  DCDeviceTest
-//
-//  Created by zhgchgli on 2025/3/1.
-//  Copyright © 2025 ZhgChgLi. All rights reserved.
-//
-
--- TestProject/Pods/Target Support Files/Pods-DCDeviceTestTests/Pods-DCDeviceTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DCDeviceTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_DCDeviceTestTests
-@end
